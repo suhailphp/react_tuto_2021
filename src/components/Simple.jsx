1e5c28f0fda5a0fba4c6eae9cc73baadcab0c7ab
suhailphp <suhailphp@gmail.com>
2021-12-22 12:12:23 +0400

    Passing props is learn

--- src/components/Simple.jsx
+++ src/components/Simple.jsx
@@ -2,8 +2,9 @@
 import React,{Component } from 'react';
 class Simple extends Component {
     render(){
+        let {name} = this.props
         return (
-            <h1>Hello mr suhail</h1>
+            <h1>Hello mr {name}</h1>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Simple />
+    <Simple name='Suhail m'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
