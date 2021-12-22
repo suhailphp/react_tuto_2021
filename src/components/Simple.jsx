5357e6e7ebad1404027bd9d97b3c196232162997
suhailphp <suhailphp@gmail.com>
2021-12-22 12:31:30 +0400

    last chagnes

--- src/components/Simple.jsx
+++ src/components/Simple.jsx
@@ -4,7 +4,11 @@ class Simple extends Component {
     render(){
         let {name} = this.props
         return (
-            <h1>Hello mr {name}</h1>
+            <div>
+                <h1>Hello mr {name}</h1>
+                <button>hello</button>
+            </div>
+
         )
     }
 }
