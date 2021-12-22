9e28cb4c7b113c819c4a493cccb69a479cefea46
suhailphp <suhailphp@gmail.com>
2021-12-22 12:10:17 +0400

    Components creating and calls in index page is done

---
+++ src/components/Simple.jsx
@@ -0,0 +1,11 @@
+
+import React,{Component } from 'react';
+class Simple extends Component {
+    render(){
+        return (
+            <h1>Hello mr suhail</h1>
+        )
+    }
+}
+
+export default Simple
--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Simple from './components/Simple'
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Simple />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
